c64c9d3d135cbfc5560289f7192291eb987e2b84
Ransika Gunasekara <56599272+RansikaG@users.noreply.github.com>
2022-11-29 12:41:05 +0530

    grayscale

--- prepare_ship.py
+++ prepare_ship.py
@@ -1,5 +1,13 @@
 import os
 from shutil import copyfile
+from PIL import Image
+
+
+# Helper function to save image as grayscale
+def save_grayscale(source, destination):
+    img = Image.open(source).convert('L')
+    img.save(destination)
+
 
 # You only need to change this line to your dataset download path
 download_path = 'F:/Downloads/archive/Ships dataset'
@@ -10,7 +18,7 @@ if not os.path.isdir(download_path):
 save_path = download_path + '/pytorch'
 if not os.path.isdir(save_path):
     os.mkdir(save_path)
-#-----------------------------------------
+# -----------------------------------------
 # #query
 # query_path = download_path + '/query'
 # query_save_path = download_path + '/pytorch/query'
@@ -66,43 +74,45 @@ if not os.path.isdir(save_path):
 #             os.mkdir(dst_path)
 #         copyfile(src_path, dst_path + '/' + name)
 
-#---------------------------------------
-#train_all
+# ---------------------------------------
+# train_all
+# This takes all the images from train and val and creates train_all folder
 train_path = download_path + '/train/images'
-train_save_path = download_path + '/pytorch/train'
-if not os.path.isdir(train_save_path):
-    os.mkdir(train_save_path)
+val_path = download_path + '/val/images'
+train_all_save_path = download_path + '/pytorch/train_all'
+if not os.path.isdir(train_all_save_path):
+    os.mkdir(train_all_save_path)
 
-for root, dirs, files in os.walk(train_path, topdown=True):
-    for name in files:
-        if not name[-3:]=='jpg':
-            continue
-        ID  = name.split('_')
-        src_path = train_path + '/' + name
-        dst_path = train_save_path + '/' + ID[0]
-        if not os.path.isdir(dst_path):
-            os.mkdir(dst_path)
-        copyfile(src_path, dst_path + '/' + name)
+for path in [train_path, val_path]:
+    for root, dirs, files in os.walk(path, topdown=True):
+        for name in files:
+            if not name[-3:] == 'jpg':
+                continue
+            ID = name.split('_')
+            src_path = path + '/' + name
+            dst_path = train_all_save_path + '/' + ID[0]
+            if not os.path.isdir(dst_path):
+                os.mkdir(dst_path)
+            save_grayscale(src_path, dst_path + '/' + name)
 
-
-#---------------------------------------
-#train_val
-train_path = download_path + '/val/images'
-train_save_path = download_path + '/pytorch/train'
-val_save_path = download_path + '/pytorch/val'
-if not os.path.isdir(train_save_path):
-    os.mkdir(train_save_path)
-    os.mkdir(val_save_path)
-
-for root, dirs, files in os.walk(train_path, topdown=True):
-    for name in files:
-        if not name[-3:]=='jpg':
-            continue
-        ID  = name.split('_')
-        src_path = train_path + '/' + name
-        dst_path = train_save_path + '/' + ID[0]
-        if not os.path.isdir(dst_path):
-            os.mkdir(dst_path)
-            dst_path = val_save_path + '/' + ID[0]  #first image is used as val image
-            os.mkdir(dst_path)
-        copyfile(src_path, dst_path + '/' + name)
+# ---------------------------------------
+# train_val
+# train_path = download_path + '/val/images'
+# train_save_path = download_path + '/pytorch/train'
+# val_save_path = download_path + '/pytorch/val'
+# if not os.path.isdir(train_save_path):
+#     os.mkdir(train_save_path)
+#     os.mkdir(val_save_path)
+#
+# for root, dirs, files in os.walk(train_path, topdown=True):
+#     for name in files:
+#         if not name[-3:]=='jpg':
+#             continue
+#         ID  = name.split('_')
+#         src_path = train_path + '/' + name
+#         dst_path = train_save_path + '/' + ID[0]
+#         if not os.path.isdir(dst_path):
+#             os.mkdir(dst_path)
+#             dst_path = val_save_path + '/' + ID[0]  #first image is used as val image
+#             os.mkdir(dst_path)
+#         copyfile(src_path, dst_path + '/' + name)
